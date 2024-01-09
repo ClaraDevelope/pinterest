dec6fa21fa55ef570833d68a728409cde7894155
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-01-09 17:35:09 +0100

    save changes

--- main.js
+++ main.js
@@ -6,22 +6,20 @@ import { drawCard } from './src/components/cards/cards.js'
 
 createCardSection()
 
-const CallApi = (inputValue) => {
+async function CallApi(inputValue) {
   console.log(inputValue)
-  const accessKey = 'g6eFRes8Scdf-v3-7NskAUI3a6ia-iN19f53RynwFy8'
+  const accessKey = '_VtxJJpqoAQc1RM-7y5fAouBNFTC22E4hVglMeLAOKg'
   const API_URL = `https://api.unsplash.com/search/photos?query=${inputValue}&client_id=${accessKey}`
-  fetch(API_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-      createValueCardSection(data, API_URL)
-      // drawCard(data, parent)
-    })
-    .catch((error) => {
-      console.log('Error al obtener los datos de la API', error)
-    })
+  try {
+    const response = await fetch(API_URL)
+    const data = await response.json()
+    console.log(data)
+    createValueCardSection(data, API_URL)
+    // drawCard(data, parent);
+  } catch (error) {
+    console.log('Error al obtener los datos de la API', error)
+  }
 }
-
 const handleKeyDown = (event) => {
   if (event.key === 'Enter') {
     const inputValue = event.target.value
@@ -31,3 +29,19 @@ const handleKeyDown = (event) => {
 
 const searchInput = document.querySelector('#searchBar')
 searchInput.addEventListener('keydown', handleKeyDown)
+
+// const CallApi = (inputValue) => {
+//   console.log(inputValue)
+//   const accessKey = 'g6eFRes8Scdf-v3-7NskAUI3a6ia-iN19f53RynwFy8'
+//   const API_URL = `https://api.unsplash.com/search/photos?query=${inputValue}&client_id=${accessKey}`
+//   fetch(API_URL)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data)
+//       createValueCardSection(data, API_URL)
+//       // drawCard(data, parent)
+//     })
+//     .catch((error) => {
+//       console.log('Error al obtener los datos de la API', error)
+//     })
+// }
--- src/components/imagesSection/images.js
+++ src/components/imagesSection/images.js
@@ -2,13 +2,12 @@ import './images.css'
 
 import { drawCard } from '../cards/cards'
 
-// COGER LA URL DE LA API + PHOTOS?PER_PAGE... (MIRA A VER CÓMO SE CONSTRUYE LA URL E INTENTA HACER UN MATH FLOOR (MATH RANDOM) DEL NÚMERO DE PÁGINA PARA QUE CADA VEZ QUE SE CARGUE, LLEGUEN IMÁGENES NUEVAS) AYÚDATE DEL GITHUB DE VIRGINIA Y DE PAULA
 export const createCardSection = (data) => {
   const app = document.querySelector('#app')
   app.innerHTML = ''
   const cardSection = document.createElement('section')
   cardSection.className = 'cardSection'
-  const USER_KEY = 'g6eFRes8Scdf-v3-7NskAUI3a6ia-iN19f53RynwFy8'
+  const USER_KEY = '_VtxJJpqoAQc1RM-7y5fAouBNFTC22E4hVglMeLAOKg'
   const randomPage = Math.floor(Math.random() * 346) + 1
   let API_URL = `https://api.unsplash.com/photos?page=${randomPage}&client_id=${USER_KEY}`
   fetch(API_URL)
@@ -21,21 +20,43 @@ export const createCardSection = (data) => {
     })
   app.append(cardSection)
 }
-// createCardSection()
 
-export const createValueCardSection = (data, API_URL) => {
+export async function createValueCardSection(data, API_URL) {
   const app = document.querySelector('#app')
   app.innerHTML = ''
   const cardSection = document.createElement('section')
   cardSection.className = 'cardSection'
 
-  fetch(API_URL)
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((item) => {
-        // console.log(item)
-        drawCard(item, cardSection)
-      })
-    })
-  app.append(cardSection)
+  try {
+    const response = await fetch(API_URL)
+    const data = await response.json()
+
+    for (const item of data) {
+      const card = await drawCard(item, cardSection)
+      cardSection.appendChild(card)
+    }
+
+    app.appendChild(cardSection)
+  } catch (error) {
+    console.error('Error fetching data from API:', error)
+  }
 }
+
+// createCardSection()
+
+// export const createValueCardSection = (data, API_URL) => {
+//   const app = document.querySelector('#app')
+//   app.innerHTML = ''
+//   const cardSection = document.createElement('section')
+//   cardSection.className = 'cardSection'
+
+//   fetch(API_URL)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       data.forEach((item) => {
+//         // console.log(item)
+//         drawCard(item, cardSection)
+//       })
+//     })
+//   app.append(cardSection)
+// }
